56ef15ae7222075ae07eb7eae422327185e0cbb5
Harald Zealot <harald_zealot@tut.by>
2013-05-02 10:28:52 +0300

    minimum function

--- main.d
+++ main.d
@@ -1,9 +1,31 @@
 module main;
 
-import std.stdio;
+import std.stdio, std.array;
+
+
 
 void main(string[] args)
 {
    writefln("Hello World\n");
+
+   writefln("min%s = %s", [-1, 2, 3], min([-1, 2, 3]));
+   writefln("min%s = %s", [3.5, 2, 1e-6], min([3.5, 2, 1e-6]));
+   writefln("min%s = %s", ["ac", "ab", "b"], min(["ac", "ab", "b"]));
    //return 0;
+}
+
+
+T min(T)(T[] input)
+{
+   if(!input.empty) {
+      auto result = input[0];
+      foreach(element; input)
+
+      if(element < result) {
+         result = element;
+      }
+
+      return result;
+   }
+   else { assert(false); }
 }
