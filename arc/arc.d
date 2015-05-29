0077e64c7cbef7ff87d5874a8c0b0ee690d9de9b
Harald Zealot <harald_zealot@tut.by>
2015-05-29 22:17:59 +0300

    [+] start arc pattern example

---
+++ arc/arc.d
@@ -0,0 +1,22 @@
+module arc;
+
+struct Arc
+{
+	int output() const
+	{
+		return a * b;
+	}
+	
+	void inputA(int val)
+	{
+		a = val;
+	}
+	
+	void inputB(int val)
+	{
+		b = val;
+	}
+	
+private:
+	int a = 1, b = 1;
+}
