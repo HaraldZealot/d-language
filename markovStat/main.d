7786222bf510a7aef91cf05f8edd39cb1f5d1694
Harald Zealot <harald_zealot@tut.by>
2015-06-03 22:28:25 +0300

    [+] markov statistics prototype

---
+++ markovStat/main.d
@@ -0,0 +1,24 @@
+﻿module main;
+
+import std.stdio;
+
+void main()
+{
+	uint[dchar][dchar] markovMatrix;
+	string text = "Просты прыклад тэкста. Занадта малы для апрацоўкі.";
+	dchar p = '\0';
+	foreach(dchar c; text)
+	{
+		++markovMatrix[p][c];
+		p=c;
+	}
+
+	foreach(row; markovMatrix.byKeyValue)
+	{
+		foreach(elem; row.value.byKeyValue)
+			write(row.key, "->", elem.key, ": ", elem.value,"\t");
+		writeln();
+	}
+
+}
+
