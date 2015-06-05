885d7c5f1ade964a14f17861d7d8a7771ef01051
Harald Zealot <harald_zealot@tut.by>
2015-06-05 21:42:08 +0300

    [+] table

--- markovStat/main.d
+++ markovStat/main.d
@@ -1,14 +1,21 @@
 ﻿module main;
 
-import std.stdio, std.string;
+import std.stdio, std.string, std.regex, std.algorithm, std.array;
 
 void main()
 {
 	uint[dchar][dchar] markovMatrix;
-	string text = "Просты прыклад тэкста. Занадта малы для апрацоўкі.";
-	dchar p = '\0';
+	string text = cast(string)stdin.byChunk(4096).joiner.array;
+	dchar p = ' ';
 
 	text = toLower(text);
+
+	auto re1 = ctRegex!(`[–.,:!?;*()0-9-]`);
+	text = replaceAll(text, re1, " ");
+	auto re2 = ctRegex!(`\s+`);
+	text = replaceAll(text, re2, " ");
+
+	//writeln(text);
 
 	foreach(dchar c; text)
 	{
@@ -16,12 +23,36 @@ void main()
 		p=c;
 	}
 
-	foreach(row; markovMatrix.byKeyValue)
+	/+foreach(row; markovMatrix.byKeyValue)
 	{
 		foreach(elem; row.value.byKeyValue)
-			write(row.key, "->", elem.key, ": ", elem.value,"\t");
+			write(row.key, "->", elem.key, ": ", elem.value,"\t\t");
+		writeln();
+	}+/
+
+	writeln("\n\n");
+
+	auto alphabet = "абвгдеёжзійклмнопрстуўфхцчш’ыьэюя ";
+
+	write(" |");
+	foreach(dchar colchar; alphabet)
+		writef("  %c|", colchar);
+	writeln();
+
+	foreach(dchar rowchar; alphabet)
+	{
+		write(rowchar,"|");
+		if(rowchar in markovMatrix)
+		{
+			foreach(dchar colchar; alphabet)
+			{
+				if(colchar in markovMatrix[rowchar])
+					writef("%3d|", markovMatrix[rowchar][colchar]);
+				else
+					write("   |");
+			}
+		}
 		writeln();
 	}
-
 }
 
